3d6d4c5e7c312b2fcd8c265bef7f08e1e928a879
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-10-30 19:24:26 +0100

    Self-attention head

--- optimus/self_attention_head.py
+++ optimus/self_attention_head.py
@@ -1,16 +1,18 @@
+import math
+
 import torch
 import torch.nn as nn
 
 
 class SelfAttentionHead(nn.Module):
-    def __init__(self, embedding_size: int, value_size: int) -> None:
+    def __init__(self, model_size: int, keyvalue_size: int) -> None:
         super().__init__()
-        self.embedding_size = embedding_size
-        self.value_size = value_size
+        self.embedding_size = model_size
+        self.value_size = keyvalue_size
 
-        self.W_q = nn.Parameter(torch.randn(embedding_size, value_size))
-        self.W_k = nn.Parameter(torch.randn(embedding_size, value_size))
-        self.W_v = nn.Parameter(torch.randn(embedding_size, value_size))
+        self.W_q = nn.Parameter(torch.randn(model_size, keyvalue_size))
+        self.W_k = nn.Parameter(torch.randn(model_size, keyvalue_size))
+        self.W_v = nn.Parameter(torch.randn(model_size, keyvalue_size))
 
     def forward(self, x):
         """Forwards self attn head
@@ -23,32 +25,8 @@ class SelfAttentionHead(nn.Module):
         k: torch.Tensor = torch.matmul(x, self.W_k)
         v = torch.matmul(x, self.W_v)
 
-        dot_prod = torch.matmul(q, k.transpose(0, 1))
-        softmaxed = torch.softmax(dot_prod, dim=1)
+        dot_prod = torch.matmul(q, k.transpose(1, 2)) / math.sqrt(self.value_size)
+        softmaxed = torch.softmax(dot_prod, dim=2)
         z = torch.matmul(softmaxed, v)
-
-        return z
-
-
-class SelfAttention(nn.Module):
-    def __init__(self, n_heads: int, embedding_size: int, value_size: int) -> None:
-        super().__init__()
-        self.heads = nn.ModuleList()
-        for _ in range(n_heads):
-            self.heads.append(SelfAttentionHead(embedding_size, value_size))
-
-        self.WO = nn.Parameter(torch.randn(n_heads * value_size, embedding_size))
-
-    def forward(self, x):
-        # x shape seq len x embedding size
-        # out shape seq len x embedding size
-
-        head_values = []
-        for h in self.heads:
-            head_values.append(h(x))
-
-        head_values = torch.cat(head_values, dim=1)
-
-        z = torch.matmul(head_values, self.WO)
 
         return z
--- tests/self_attn_head_test.py
+++ tests/self_attn_head_test.py
@@ -1,54 +1,58 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+import pytest
 import torch
 import torch.nn as nn
+from assertpy import assert_that
 
-from optimus.self_attention_head import SelfAttention, SelfAttentionHead
-
-
-def test_self_attn_head_has_correct_shape_for_no_batch():
-    embed_size = 589
-    val_size = 124
-    seq_len = 512
-    data = torch.randn(seq_len, embed_size)
-
-    attn_head = SelfAttentionHead(embed_size, val_size)
-    result = attn_head(data)
-
-    assert result.shape == (seq_len, val_size)
+from optimus.self_attention_head import SelfAttentionHead
 
 
-def test_self_attn_head_is_training_params():
-    embed_size = 128
-    val_size = 64
-    seq_len = 512
-    data = torch.randn(seq_len, embed_size)
-    y = torch.randn(seq_len, val_size)
-    criterion = nn.MSELoss()
+class TestSelfAttention:
+    def _get_tensor_with_shape(self, *shape):
+        return torch.randn(*shape)
 
-    attn_head = SelfAttentionHead(embed_size, val_size)
-    result = attn_head(data)
-    loss = criterion(y, result)
-    loss.backward()
+    def assert_tensor_has_shape(self, tensor: torch.Tensor, shape: Tuple[int]):
+        assert_that(tensor).is_type_of(torch.Tensor)
+        assert_that(tensor.shape).is_equal_to(shape)
 
-    assert attn_head.W_q.grad is not None
-    assert attn_head.W_k.grad is not None
-    assert attn_head.W_v.grad is not None
+    @pytest.mark.parametrize(
+        "batch_size,max_seq_len,model_size,keyvalue_size",
+        [(16, 25, 30, 12), (1, 25, 30, 12)],
+    )
+    def test_should_return_correct_shape(
+        self, batch_size, max_seq_len, model_size, keyvalue_size
+    ):
+        data = self._get_tensor_with_shape(batch_size, max_seq_len, model_size)
+        attn_head = SelfAttentionHead(
+            model_size=model_size, keyvalue_size=keyvalue_size
+        )
 
+        result = attn_head(data)
 
-def test_self_attn_has_correct_shape_for_no_batch():
-    embed_size = 32
-    val_size = 12
-    seq_len = 128
-    n_heads = 6
-    data = torch.randn(seq_len, embed_size)
-    y = torch.randn(seq_len, embed_size)
-    criterion = nn.MSELoss()
+        self.assert_tensor_has_shape(result, (batch_size, max_seq_len, keyvalue_size))
 
-    self_attn = SelfAttention(n_heads, embed_size, val_size)
-    result = self_attn(data)
-    loss = criterion(y, result)
-    loss.backward()
+    @pytest.mark.parametrize(
+        "batch_size,max_seq_len,model_size,keyvalue_size",
+        [(16, 25, 30, 12), (1, 25, 30, 12)],
+    )
+    def test_should_have_grad_in_params_after_backprop(
+        self, batch_size, max_seq_len, model_size, keyvalue_size
+    ):
+        x = self._get_tensor_with_shape(batch_size, max_seq_len, model_size)
+        y = self._get_tensor_with_shape(batch_size, max_seq_len, keyvalue_size)
 
-    assert self_attn.WO.grad is not None
-    for h in self_attn.heads:
-        for name, param in h.named_parameters():
-            param.grad is not None
+        attn_head = SelfAttentionHead(
+            model_size=model_size, keyvalue_size=keyvalue_size
+        )
+
+        result = attn_head(x)
+
+        criterion = nn.MSELoss()
+        loss = criterion(y, result)
+        loss.backward()
+
+        assert attn_head.W_q.grad is not None
+        assert attn_head.W_k.grad is not None
+        assert attn_head.W_v.grad is not None
